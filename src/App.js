c90650e4bbc87d15ebc050610d22e1b0e740d1b9
DesignerSamadhan <36732154+DesignerSamadhan@users.noreply.github.com>
2021-10-17 19:42:36 +0530

    Initial App start files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { InventoryList } from './_components/InventoryModule';
+
+function App() {
+  return (
+    <div>
+      <InventoryList />
+    </div>
+  );
+}
+
+export default App;
