88b31aac5b35a6fbcd3434fe729a95e4346add8f
DesignerSamadhan <36732154+DesignerSamadhan@users.noreply.github.com>
2021-10-17 19:44:50 +0530

    Create index.js

---
+++ src/_reducers/index.js
@@ -0,0 +1,8 @@
+import { InventoryItems } from './inventory.reducer';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    InventoryItems   
+  });
+
+  export default rootReducer;
